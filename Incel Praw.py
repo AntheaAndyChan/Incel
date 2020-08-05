e95c8dcb1feae5b7ff4b2af18983e29f0b70cc8f
Anthea Chan <ac4131@nyu.edu>
2020-08-05 17:29:21 -0400

    CSV instead of json
    saves data as dataframes and csv instead of json.

--- Incel Praw.py
+++ Incel Praw.py
@@ -5,7 +5,8 @@ Created on Tue Apr  7 11:59:46 2020
 @author: Chan234
 """
 import praw
-import json
+import pandas as pd
+# import json
 
 reddit=praw.Reddit(client_id= 'AHg4WHV1FIjPig',
                    client_secret= 'pi7ttIzSPGcOt8B4VUtLYvkA8fg',
@@ -13,50 +14,101 @@ reddit=praw.Reddit(client_id= 'AHg4WHV1FIjPig',
                    password= 'Greta-Andy-Kaylee2020',
                    user_agent= 'Incel Research')
 
-def get_hot_posts(sub):
-    return  sub.hot(limit=100)
+# We now want to get all posts from incelexit specifically, and save them into a dataframe to export to csv later
+subreddit = reddit.subreddit('IncelExit')
+submission_title = []
+submission_ID = []
+ups = []
+body = []
+time = []
+num_comments = []
+link = []
+redditor = []
+i=0
 
-def jsonify_post(post, file_object):
-    post_json = {
-        'Post Title': post.title,
-        'Post ID': post.id,
-        'Post Upvotes': post.ups,
-        'Post Downvotes': post.downs,
-        'Post Body': post.selftext
-        } 
-    return post_json
 
-def jsonify_comment(comment, file_object):
-    c_out = {
-        'Parent ID': str(comment.parent()), #parents can be the submission, or the comment
-        'Comment ID': comment.id,
-        'Comment Body': comment.body
-        } 
-    return c_out
-     
-def savehotsubreddit(subreddit):   
-    incel_sub=reddit.subreddit(subreddit)
+for submission in subreddit.top(limit=1000):
+    i+=1
+    submission_title.append(submission.title)
+    submission_ID.append(submission.id)
+    time.append(submission.created_utc)
+    ups.append(submission.ups)
+    body.append(submission.selftext)
+    num_comments.append(submission.num_comments)
+    link.append(submission.permalink)
+    redditor.append(submission.author) 
+    if i%5 == 0:
+        print(f'{i} submissions completed')
     
-    hot_posts = get_hot_posts(incel_sub) 
-    for post in hot_posts:
-        post_path = f'E:\\Work\\Incels\\Scraping\\{subreddit}\\{post.id}.txt'
-        post_file = open(post_path, "w+") 
-        post_json = jsonify_post(post, post_file)
-         
-        post.comments.replace_more(limit=None)
-        comments = post.comments.list()
-        comment_json = []
-        for comment in comments:
-            #check if comment contains bots 
-            bot1= "I'm a bot"
-            bot2= "^^I'm ^^a ^^bot"
-            com=comment.body
-            if bot1 not in com and bot2 not in com:
-                comment_json.append(jsonify_comment(comment, post_file))
-        post_json['Comments'] = comment_json
-        post_file.writelines(json.dumps(post_json,  indent=4))
-        post_file.close()
- 
-subreddits= ['ricecels','hapacels','IncelExit','IncelswithoutHate','IncelsInAction','TruFemCels','AskTruFemCels']
-for sub in subreddits:
-    savehotsubreddit(sub)
+posts_df = pd.DataFrame(
+    {'submission_title': submission_title, 
+        'submission_ID': submission_ID,
+        'redditor': redditor,
+        'ups': ups,
+        'body': body,
+        'time': time,
+        'num_comments': num_comments,
+        'link': link
+    })
+
+posts_df.head(10)
+
+#comments
+submission_title = []
+parent_ID = []
+submission_ID = []
+comment_ID = []
+body = []
+redditor = []
+ups = []
+time = []
+is_submitter=[]
+link = []
+subs=0
+bot1= "I'm a bot"
+bot2= "^^I'm ^^a ^^bot" 
+
+for submission in subreddit.top(limit=1000):
+    subs+=1
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list(): 
+       #check if comment contains bots 
+       if bot1 not in comment.body and bot2 not in comment.body: 
+            submission_ID.append(submission.id)
+            submission_title.append(submission.title)
+            parent_ID.append(str(comment.parent()))
+            comment_ID.append(comment.id)
+            redditor.append(comment.author)
+            is_submitter.append(comment.is_submitter)
+            body.append(comment.body)
+            ups.append(comment.ups)
+            link.append(submission.permalink)
+            time.append(comment.created_utc)
+    if subs%5 == 0:
+        print(f'{subs} submissions comments completed')
+    
+        
+comments_df = pd.DataFrame(
+    {'submission_ID': submission_ID,
+        'submission_title': submission_title, 
+        'parent_ID': parent_ID,
+        'comment_ID': comment_ID,
+        'redditor': redditor,
+        'ups': ups,
+        'body': body,
+        'time': time,
+        'body': body,
+        'is_submitter':is_submitter,
+        'link': link
+    })
+
+comments_df.head(10)
+
+#merge comments and post dfs, sort by submission ID then parent ID then comment ID, then export to csv
+all_content = [posts_df, comments_df]
+result = pd.concat(all_content)
+result.to_csv (r'E:\Work\Incels\Scrape_August 2020\IncelExit.csv', index = False, header=True)
+
+#When we want to start investigating prolific posters, 
+#check out this link: https://praw.readthedocs.io/en/latest/code_overview/models/redditor.html
+#once we get the csv we can get a frequency of redditors! 
